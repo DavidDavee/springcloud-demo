58c039cee5b828ae71c62b336aa6113ab2e72b50
DavidDavee <41644633+DavidDavee@users.noreply.github.com>
2024-05-16 01:06:30 +0800

    update nacos

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/demo/cloud/controller/PayAlibabaController.java
@@ -0,0 +1,24 @@
+package com.demo.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: springcloud-demo
+ * @description:
+ * @author: David
+ * @create: 2024-05-16 01:02
+ **/
+@RestController
+public class PayAlibabaController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/pay/nacos/{id}")
+    public String getPayInfo(@PathVariable("id") Integer id)
+    {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}
